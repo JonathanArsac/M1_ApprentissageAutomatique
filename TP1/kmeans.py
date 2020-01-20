a7d8afa5325c8ec20b34ba5266f7cec278fb5d72
Jaune <jonathanarsac@orange.fr>
2020-01-20 14:52:36 +0100

    Mise sur git des cours et du premier TP

---
+++ TP1/kmeans.py
@@ -0,0 +1,120 @@
+# author: Benjamin Chamand, Thomas Pelligrini
+
+import shutil
+from typing import Tuple
+
+import numpy as np
+
+from utils import plot_training
+
+
+class KMeans(object):
+    def __init__(self, n_clusters:int, max_iter:int, early_stopping:bool=False,
+                 tol:float=1e-4, display:bool=False) -> None:
+        self.n_clusters = n_clusters            # Nombre de clusters
+        self.max_iter = max_iter                # Nombre d'itération
+        self.early_stopping = early_stopping    # arrête l'apprentissage si 
+        self.tol = tol                          # seuil de tolérance entre 2 itérations
+        self.display = display                  # affichage des données
+
+        self.cluster_centers = None             # Coordonnées des centres de regroupement
+                                                # (centre de gravité des classes)
+    
+    def _compute_distance(self, vec1:np.ndarray, vec2:np.ndarray) -> Tuple(float, np.ndarray):
+        """Retourne la distance quadratique entre vec1 et vec2 (squared euclidian distance)
+        """
+        pass
+    
+    def _compute_inertia(self, X:np.ndarray, y:np.ndarray) -> float:
+        """Retourne la Sum of Squared Errors entre les points et le centre de leur
+        cluster associe
+        """
+        pass
+    
+    def _update_centers(self, X:np.ndarray, y:np.ndarray) -> None:
+        """Recalcule les coordonnées des centres des clusters
+        """
+        pass
+
+    def predict(self, X:np.ndarray) -> np.ndarray:
+        """attribue un indice de cluster à chaque point de data
+
+        X = données
+        y = cluster associé à chaque donnée
+        """
+        # nombre d'échantillons
+        n_data = X.shape[0]
+
+        pass
+
+    def fit(self, X:np.ndarray) -> None:
+        """Apprentissage des centroides
+        """
+        # Récupère le nombre de données
+        n_data = X.shape[0]
+
+        # Sauvegarde tous les calculs de la somme des distances euclidiennes pour l'affichage
+        if self.display:
+            shutil.rmtree('./img_training', ignore_errors=True)
+            metric = []
+
+        # 2 cas à traiter : 
+        #   - soit le nombre de clusters est supérieur ou égale au nombre de données
+        #   - soit le nombre de clusters est inférieur au nombre de données
+        if self.n_clusters >= n_data:
+            # Initialisation des centroides : chacune des données est le centre d'un clusteur
+            self.cluster_centers = np.zeros(self.n_clusters, X.shape[1])
+            self.cluster_centers[:n_data] = X
+        else:
+            # Initialisation des centroides
+            self.cluster_centers = X[:self.n_clusters,:]
+
+            # initialisation d'un paramètre permettant de stopper les itérations lors de la convergence
+            stabilise = False
+
+            # Exécution de l'algorithme sur plusieurs itérations
+            for i in range(self.max_iter):
+                # détermine le numéro du cluster pour chacune de nos données
+                y = self.predict(X)
+
+                # calcule de la somme des distances initialiser le paramètres
+                # de la somme des distances
+                if i == 0:
+                    current_distance = self._compute_inertia(X, y)
+
+                # mise à jour des centroides
+                self._update_centers(X, y)
+
+                # mise à jour de la somme des distances
+                old_distance = current_distance
+                current_distance = self._compute_inertia(X, y)
+
+                # stoppe l'algorithme si la somme des distances quadratiques entre 
+                # 2 itérations est inférieur au seuil de tolérance
+                if self.early_stopping:
+                    # A compléter
+                    #stabilise = ....
+                    if stabilise:
+                        break
+
+                # affichage des clusters
+                if self.display:
+                    diff = abs(old_distance - current_distance)
+                    metric.append(diff)
+                    plot_training(i, X, y, self.cluster_centers, metric)
+
+    def score(self, X:np.ndarray, y:np.ndarray) -> float:
+        """Calcule le score de pureté
+        """
+        n_data = X.shape[0]
+
+        y_pred = self.predict(X)
+
+        score = 0
+        for i in range(self.n_clusters):
+            _, counts = np.unique(y[y_pred == i], return_counts=True) 
+            score += counts.max()
+
+        score /= n_data
+
+        return score
---
+++ TP1/main_kmeans.py
@@ -0,0 +1,32 @@
+# author: Benjamin Chamand, Thomas Pelligrini
+
+from kmeans import KMeans
+from utils import load_dataset
+
+
+def main():
+    filepath = "./data/self_test.csv"
+    #filepath = "./data/iris.csv"
+
+    # chargement des données
+    data, labels = load_dataset(filepath)
+
+    # initialisation de l'objet KMeans
+    kmeans = KMeans(n_clusters=3,
+                    max_iter=100,
+                    early_stopping=True,
+                    tol=1e-6,
+                    display=True)
+
+    # calcule les clusters
+    kmeans.fit(data)
+
+    # calcule la pureté de nos clusters
+    score = kmeans.score(data, labels)
+    print("Pureté : {}".format(score))
+
+    input("Press any key to exit...")
+
+
+if __name__ == "__main__":
+    main()
